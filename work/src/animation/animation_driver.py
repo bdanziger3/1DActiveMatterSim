5d0b6253ecde9168b679e72d4e7e555829e3e7bd
Blake Danziger <blake.danziger@gmail.com>
2025-08-01 01:31:01 +0100

    animate some in dir

--- work/src/animation/animation_driver.py
+++ work/src/animation/animation_driver.py
@@ -10,7 +10,7 @@ import moviepy
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from simulation.sim_structs import SimulationData, SimulationParameters
-from file_management.simdata_files import loadsim, loadsim_n_lines, prepare_simfile
+from file_management.simdata_files import loadsim, loadsim_n_lines, get_simfile_type, prepare_simfile
 from animation.video_file import resize_videofile
 from animation.simdata_redblue_animation import sim_animate, make_mp4s_of_dir, sim_animate_follow
 from animation.density_hist_animation import particle_density_animate
@@ -35,14 +35,52 @@ from utils.paths import fix_path
 # final_clip.write_videofile(new_clip_filepath)
 
 
-file_align = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01.txt")
-save_filepath = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01.mp4")
-save_filepath_faster = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01_faster.mp4")
-particle_density_animate(file_align, 100, False, True, fps=40, delete_gif=True)
+# file_align = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01.txt")
+# save_filepath = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01.mp4")
+# save_filepath_faster = fix_path("work/data/26-6/N1000-B100-alignsimple-300-t100-sn0.01_faster.mp4")
+# particle_density_animate(file_align, 100, False, True, fps=40, delete_gif=True)
+
+file_turnaway_dir = fix_path("work/data/sweeps/turnaway/interactionsweep/N1000-B100")
+file_turnaway_dir2 = fix_path("work/data/sweeps/turnaway/interactionsweep/smaller")
+file_turnaway_dir3 = fix_path("work/data/sweeps/turnaway/interactionsweep/smaller/small-interaction")
+file_turnaway_dir4 = fix_path("work/data/sweeps/turnaway/boxwidthsweep/small_int/d100")
+# int100_box10 = fix_path("work/data/sweeps/turnaway/boxwidthsweep/N100-B10.0-turnaway-100.txt")
+t1000 = fix_path("work/data/sweeps/alignsimple/interactionsweep/extended/N1000-B100.0-alignsimple-150_t1000.txt")
+# file_turnaway_1 = fix_path("work/data/sweeps/turnaway/interactionsweep/smaller/N1000-B100.0-turnaway-1_DES.txt")
+# make_mp4s_of_dir(file_turnaway_dir4)
+
+simdir = fix_path("work/data/sweeps/alignsimple/interactionsweep/extended")
+
+file_list = os.listdir(simdir)
+print(file_list)
+for file_name in file_list:
+    if file_name.endswith(".txt"):
+
+        full_file_path = os.path.join(simdir, file_name)
+        prepared_file_path = prepare_simfile(full_file_path)
+        print(f"Animating and Saving {prepared_file_path}...")
+        
+        try:
+            sim_animate(prepared_file_path, show=False, save=True, fps=10, y_offset=True, delete_gif=True)
+            print("Done")
+        except:
+            print(f"Could not animate sim at {file_name}.")
+
+        # delete prepared file if new
+        if prepared_file_path != full_file_path:
+            print(f"Deleting {prepared_file_path}...")
+            os.remove(prepared_file_path)
+            print("Done")
 
 
-clip = moviepy.VideoFileClip(save_filepath)
-faster_clip = resize_videofile(clip, 60, 30, save_filepath_faster)
+# for file in 
+# sim_animate(t1000_prepared, show=False, save=True, fps=10, y_offset=True, delete_gif=True)
+
+# sim_animate_follow(int100_box10_prepared, 0, show=False, save=True, fps=10, y_offset=True, delete_gif=True)
+
+
+# clip = moviepy.VideoFileClip(save_filepath)
+# faster_clip = resize_videofile(clip, 60, 30, save_filepath_faster)
 
 
 
